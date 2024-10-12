e20eeca67d91af7d709072cb635c3b6722b86225
jeongtae Lee <sms3025@ajou.ac.kr>
2024-10-12 14:48:08 +0900

    feat: first commit
    새로운

---
+++ src/main/java/LinkerBell/campus_market_spring/CampusMarketSpringApplication.java
@@ -0,0 +1,13 @@
+package LinkerBell.campus_market_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusMarketSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusMarketSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LinkerBell/campus_market_spring/CampusMarketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package LinkerBell.campus_market_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusMarketSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
