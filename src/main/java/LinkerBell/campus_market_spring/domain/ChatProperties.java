95ecf2408885d3fedcd4813335562251b78c84bb
sms3025 <101379994+sms3025@users.noreply.github.com>
2024-10-14 18:59:40 +0900

    feat: entity추가 (#2)

---
+++ src/main/java/LinkerBell/campus_market_spring/domain/ChatProperties.java
@@ -0,0 +1,27 @@
+package LinkerBell.campus_market_spring.domain;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+public class ChatProperties extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long chatPropertiesId;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    private boolean isAlarm;
+
+    private String title;
+
+    private boolean isExited;
+}
---
+++ src/main/java/LinkerBell/campus_market_spring/domain/UserAndTerms.java
@@ -0,0 +1,23 @@
+package LinkerBell.campus_market_spring.domain;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+public class UserAndTerms extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userAndTermsId;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "terms_id")
+    private Terms terms;
+
+    private boolean isAgree;
+}
