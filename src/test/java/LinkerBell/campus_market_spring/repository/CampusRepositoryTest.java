b9aaf7bc1dc30782aea1ffa3fd73c33f317b3e28
kwon204 <dafeg3@naver.com>
2024-10-25 00:36:30 +0900

    feat: 캠퍼스 기능 추가
    캠퍼스 등록, 리스트 보기 추가
    서버 실행 시 대학, 이메일 데이터 주입 기능 추가

---
+++ src/test/java/LinkerBell/campus_market_spring/repository/CampusRepositoryTest.java
@@ -0,0 +1,62 @@
+package LinkerBell.campus_market_spring.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import LinkerBell.campus_market_spring.domain.Campus;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class CampusRepositoryTest {
+    @Autowired
+    CampusRepository campusRepository;
+
+    @BeforeEach
+    public void setUp() {
+        Campus campus = createCampus();
+        Campus campus2 = createCampus2();
+        campusRepository.save(campus);
+        campusRepository.save(campus2);
+    }
+
+    @Test
+    @DisplayName("이메일에 맞는 캠퍼스 정보 가져오기 테스트")
+    public void findByEmailTest() {
+        // given
+        String email = "ajou.ac.kr";
+        // when
+        List<Campus> campusList = campusRepository.findByEmail(email);
+        // then
+        assertThat(campusList.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 이메일 테스트")
+    public void findByEmailButNoneTest() {
+        // given
+        String email = "abc.ac.kr";
+        // when
+        List<Campus> campusList = campusRepository.findByEmail(email);
+        // then
+        assertThat(campusList.size()).isEqualTo(0);
+        System.out.println(campusList);
+    }
+
+    private Campus createCampus() {
+        return Campus.builder()
+            .email("ajou.ac.kr")
+            .region("수원시")
+            .universityName("아주대학교").build();
+    }
+
+    private Campus createCampus2() {
+        return Campus.builder()
+            .email("ajou.ac.kr")
+            .region("수원시2")
+            .universityName("아주대학교2").build();
+    }
+}
--- src/test/java/LinkerBell/campus_market_spring/service/ProfileServiceTest.java
+++ src/test/java/LinkerBell/campus_market_spring/service/ProfileServiceTest.java
@@ -4,11 +4,18 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 
+import LinkerBell.campus_market_spring.domain.Campus;
 import LinkerBell.campus_market_spring.domain.Role;
 import LinkerBell.campus_market_spring.domain.User;
+import LinkerBell.campus_market_spring.dto.CampusResponseDto;
 import LinkerBell.campus_market_spring.dto.ProfileResponseDto;
+import LinkerBell.campus_market_spring.global.error.ErrorCode;
+import LinkerBell.campus_market_spring.global.error.exception.CustomException;
+import LinkerBell.campus_market_spring.repository.CampusRepository;
 import LinkerBell.campus_market_spring.repository.UserRepository;
+import java.util.List;
 import java.util.Optional;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,6 +28,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class ProfileServiceTest {
     @Mock
     UserRepository userRepository;
+
+    @Mock
+    CampusRepository campusRepository;
 
     @InjectMocks
     ProfileService profileService;
@@ -86,13 +96,86 @@ class ProfileServiceTest {
         assertThat(profileResponseDto.getProfileImage()).isEqualTo("new_url");
     }
 
+    @Test
+    @DisplayName("캠퍼스 정보 가져오기 테스트")
+    public void getCampusTest() {
+        // given
+        User user = createUser();
+        Campus campus = createCampus();
+        given(campusRepository.findByEmail(Mockito.anyString())).willReturn(Lists.newArrayList(campus));
+        given(userRepository.findById(Mockito.anyLong())).willReturn(Optional.ofNullable(user));
+        // when
+        List<CampusResponseDto> campusList = profileService.getCampusList(user.getUserId());
+
+        // then
+        assertThat(campusList.size()).isGreaterThan(0);
+    }
+
+//    @Test
+//    public void findCampusTest() {
+//        // given
+//        String schoolEmail = "abc@asd.ac.ad";
+//        given(campusRepository.findByEmail(Mockito.anyString())).willReturn(Lists.newArrayList());
+//        // when & then
+//        assertThatThrownBy(() -> profileService.findCampusList(schoolEmail)).isInstanceOf(CustomException.class);
+//    }
+
+    @Test
+    @DisplayName("캠퍼스 저장 예외 테스트")
+    public void saveCampusErrorTest() {
+        // given
+        User user = createUser();
+        Campus campus = createCampus();
+        Campus diffCampus = createDiffCampus();
+
+        given(userRepository.findById(Mockito.anyLong())).willReturn(Optional.ofNullable(user));
+        given(campusRepository.findByEmail(Mockito.anyString())).willReturn(Lists.newArrayList(diffCampus));
+        // when & then
+        assertThatThrownBy(() -> profileService.saveCampus(user.getUserId(), campus.getCampusId()))
+            .isInstanceOf(CustomException.class);
+    }
+
+    @Test
+    @DisplayName("캠퍼스 저장 테스트")
+    public void saveCampusTest() {
+        // given
+        User user = createUser();
+        Campus campus = createCampus();
+        given(userRepository.findById(Mockito.anyLong())).willReturn(Optional.ofNullable(user));
+        given(campusRepository.findByEmail(Mockito.anyString())).willReturn(Lists.newArrayList(campus));
+        given(campusRepository.findById(Mockito.anyLong())).willReturn(Optional.ofNullable(campus));
+        // when
+        ProfileResponseDto profileResponseDto = profileService.saveCampus(user.getUserId(), campus.getCampusId());
+
+        // then
+        assertThat(profileResponseDto).isNotNull();
+    }
+
+    private Campus createCampus() {
+        return Campus.builder()
+            .campusId(1L)
+            .email("ajou.ac.kr")
+            .region("수원시")
+            .universityName("아주대학교").build();
+    }
+
+
     private User createUser() {
         return User.builder()
             .loginEmail("abc@gmail.com")
             .userId(1L)
             .nickname("old_nickname")
             .profileImage("old_url")
+            .schoolEmail("abc@ajou.ac.kr")
             .role(Role.GUEST).build();
+    }
+
+    private Campus createDiffCampus() {
+        return Campus.builder()
+            .campusId(10L)
+            .email("test.ac.kr")
+            .region("수원시")
+            .universityName("테스트대학교").build();
     }
 
 }
