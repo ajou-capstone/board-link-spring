0ed4b4b560667b87f1844447bed9fc95e71c9ba9
kwon204 <32539398+kwon204@users.noreply.github.com>
2024-11-07 13:18:16 +0900

    fix: 약관 동의 기능 버그 수정 (#39)

--- src/main/java/LinkerBell/campus_market_spring/dto/TermsRequestDto.java
+++ src/main/java/LinkerBell/campus_market_spring/dto/TermsRequestDto.java
@@ -1,12 +1,18 @@
 package LinkerBell.campus_market_spring.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TermsRequestDto {
 
-    private Long id;
+    private Long termId;
 
     @JsonProperty(value = "isAgree")
     private boolean isAgree;
