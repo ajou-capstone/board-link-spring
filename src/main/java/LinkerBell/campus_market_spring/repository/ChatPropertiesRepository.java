a44f5b502de1b1768aaae462445471f96e7e4822
zihang <tlswlgkd565@gmail.com>
2024-11-07 19:49:22 +0900

    chore : 불필요한 import 구문들 제거

--- src/main/java/LinkerBell/campus_market_spring/repository/ChatPropertiesRepository.java
+++ src/main/java/LinkerBell/campus_market_spring/repository/ChatPropertiesRepository.java
@@ -4,9 +4,6 @@ import LinkerBell.campus_market_spring.domain.ChatProperties;
 import LinkerBell.campus_market_spring.domain.ChatRoom;
 import LinkerBell.campus_market_spring.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
--- src/main/java/LinkerBell/campus_market_spring/repository/ChatRoomRepository.java
+++ src/main/java/LinkerBell/campus_market_spring/repository/ChatRoomRepository.java
@@ -3,9 +3,6 @@ package LinkerBell.campus_market_spring.repository;
 import LinkerBell.campus_market_spring.domain.ChatRoom;
 import LinkerBell.campus_market_spring.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
