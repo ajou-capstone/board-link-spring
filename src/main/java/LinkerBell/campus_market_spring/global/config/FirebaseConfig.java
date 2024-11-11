36b229d4652885525b9add414caf2df1b9f93760
jeongtae Lee <sms3025@ajou.ac.kr>
2024-11-11 19:44:33 +0900

    refactor: 직접적인 경로를 value path로 변경

--- src/main/java/LinkerBell/campus_market_spring/global/config/FirebaseConfig.java
+++ src/main/java/LinkerBell/campus_market_spring/global/config/FirebaseConfig.java
@@ -1,21 +1,28 @@
 package LinkerBell.campus_market_spring.global.config;
 
+import LinkerBell.campus_market_spring.global.error.ErrorCode;
+import LinkerBell.campus_market_spring.global.error.exception.CustomException;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class FirebaseConfig {
+
+    @Value("${firebase.json_path}")
+    String firebasePath;
 
     @PostConstruct
     public void init() {
         try {
-            InputStream serviceAccount = new FileInputStream(
-                "src/main/resources/campus-market-6bce6-firebase-adminsdk-vhvpy-5852dcffc7.json");
+            InputStream serviceAccount = new FileInputStream(firebasePath);
             FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
@@ -24,7 +31,8 @@ public class FirebaseConfig {
                 FirebaseApp.initializeApp(options);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("FireBaseConfig Error = {}",e.getMessage());
+            throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
     }
 }
