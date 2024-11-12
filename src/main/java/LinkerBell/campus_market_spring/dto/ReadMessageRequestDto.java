2a7f7aaec835446ae9e8e5ef0444c1260312573c
zihang <tlswlgkd565@gmail.com>
2024-11-12 15:17:36 +0900

    feat : null체크 기능, requestDto 추가

---
+++ src/main/java/LinkerBell/campus_market_spring/dto/ReadMessageRequestDto.java
@@ -0,0 +1,11 @@
+package LinkerBell.campus_market_spring.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ReadMessageRequestDto {
+
+    private Long messageId;
+}
