3bf54796128b7d60d562c649353342a9a659590d
zihang98 <81303872+zihang98@users.noreply.github.com>
2024-11-12 23:10:44 +0900

    feat : dto에 setter 및 생성자 추가, 메시지 발송 경로 수정 (#55)

--- src/main/java/LinkerBell/campus_market_spring/dto/ChattingResponseDto.java
+++ src/main/java/LinkerBell/campus_market_spring/dto/ChattingResponseDto.java
@@ -2,12 +2,19 @@ package LinkerBell.campus_market_spring.dto;
 
 import LinkerBell.campus_market_spring.domain.ContentType;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChattingResponseDto {
+
     private Long chattingId;
     private Long chatRoomId;
     private Long userId;
