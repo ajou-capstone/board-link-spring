bacf66b4d643862f0a34deda4a395403a65f2861
zihang <tlswlgkd565@gmail.com>
2024-11-14 01:32:05 +0900

    feat : WebSocketHandler 추가

--- src/main/java/LinkerBell/campus_market_spring/global/config/WebSocketConfig.java
+++ src/main/java/LinkerBell/campus_market_spring/global/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package LinkerBell.campus_market_spring.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,7 +10,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @EnableWebSocketMessageBroker
 @Configuration
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final WebSocketHandler webSocketHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -20,5 +25,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/sub");
         registry.setApplicationDestinationPrefixes("/send");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(webSocketHandler);
     }
 }
