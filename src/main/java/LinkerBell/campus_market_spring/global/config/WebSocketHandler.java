ec1b1062ad0296f36aebe9694c78a2731486e187
zihang <tlswlgkd565@gmail.com>
2024-11-14 15:59:27 +0900

    fix : Connect 시에 유저 정보를 저장하는 방법을 SecurityContextHolder 이용하는 방법으로 변경

--- src/main/java/LinkerBell/campus_market_spring/global/config/WebSocketHandler.java
+++ src/main/java/LinkerBell/campus_market_spring/global/config/WebSocketHandler.java
@@ -1,8 +1,5 @@
 package LinkerBell.campus_market_spring.global.config;
 
-import LinkerBell.campus_market_spring.global.error.ErrorCode;
-import LinkerBell.campus_market_spring.global.error.exception.CustomException;
-import LinkerBell.campus_market_spring.global.jwt.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
@@ -11,14 +8,13 @@ import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class WebSocketHandler implements ChannelInterceptor {
-
-    private final JwtUtils jwtUtils;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
@@ -27,17 +23,10 @@ public class WebSocketHandler implements ChannelInterceptor {
         log.info("accessor command : {}", accessor.getCommand().name());
 
         if (accessor.getCommand() == StompCommand.CONNECT) {
-            String authToken = accessor.getFirstNativeHeader("Authorization");
-
-            if (authToken == null) {
-                log.info("authToken is null");
-                throw new CustomException(ErrorCode.JWT_IS_NULL);
-            } else if (!jwtUtils.validateToken(authToken)) {
-                log.info("authToken is not valid");
-                throw new CustomException(ErrorCode.INVALID_JWT);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null) {
+                log.info("authentication is null");
             }
-
-            Authentication authentication = jwtUtils.getAuthentication(authToken);
             accessor.setUser(authentication);
             log.info("connected authentication name : {}", authentication.getName());
         }
