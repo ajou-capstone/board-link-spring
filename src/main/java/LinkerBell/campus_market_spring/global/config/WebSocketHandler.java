79dc271415ed2a14238cfc51d2893bead9c19d34
zihang98 <81303872+zihang98@users.noreply.github.com>
2024-11-14 22:52:48 +0900

    fix : StompHeaderAccessor에서 MessageHeaderAccessor를 사용해서 header에 접근하는 것으로 변경 (#71)

--- src/main/java/LinkerBell/campus_market_spring/global/config/WebSocketHandler.java
+++ src/main/java/LinkerBell/campus_market_spring/global/config/WebSocketHandler.java
@@ -11,6 +11,7 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -25,7 +26,8 @@ public class WebSocketHandler implements ChannelInterceptor {
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message,
+            StompHeaderAccessor.class);
 
         log.info("accessor command : {}", accessor.getCommand().name());
 
