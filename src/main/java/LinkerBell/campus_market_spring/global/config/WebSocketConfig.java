b211719825c9e3bc272438ff222733da8169a9d8
zihang98 <81303872+zihang98@users.noreply.github.com>
2024-11-16 16:54:37 +0900

    feat : outboundchannel에서 postsend추가 (#92)

--- src/main/java/LinkerBell/campus_market_spring/global/config/WebSocketConfig.java
+++ src/main/java/LinkerBell/campus_market_spring/global/config/WebSocketConfig.java
@@ -1,15 +1,22 @@
 package LinkerBell.campus_market_spring.global.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Slf4j
 @Order(Ordered.HIGHEST_PRECEDENCE + 99)
 @EnableWebSocketMessageBroker
 @Configuration
@@ -36,5 +43,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(webSocketHandler);
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+                // Use SimpMessageHeaderAccessor to access STOMP headers
+                SimpMessageHeaderAccessor accessor = SimpMessageHeaderAccessor.wrap(message);
+                if (accessor.getMessageType() == SimpMessageType.CONNECT_ACK) {
+                    log.info("CONNECTED event detected. Session ID: {}", accessor.getSessionId());
+                }
+            }
+        });
     }
 }
