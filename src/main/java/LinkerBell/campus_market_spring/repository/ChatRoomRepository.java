4ac2ccb1b95c3bca7721d5d56386dd76c60cd51b
zihang <tlswlgkd565@gmail.com>
2024-11-16 22:20:44 +0900

    chore : 사용하지 않는 import들 삭제

--- src/main/java/LinkerBell/campus_market_spring/repository/ChatRoomRepository.java
+++ src/main/java/LinkerBell/campus_market_spring/repository/ChatRoomRepository.java
@@ -1,15 +1,12 @@
 package LinkerBell.campus_market_spring.repository;
 
 import LinkerBell.campus_market_spring.domain.ChatRoom;
-import LinkerBell.campus_market_spring.domain.Item;
 import LinkerBell.campus_market_spring.domain.User;
-import java.util.Optional;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
