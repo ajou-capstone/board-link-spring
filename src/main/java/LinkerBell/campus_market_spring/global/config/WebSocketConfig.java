8d67cde2bd8ea3b5b5c60df6c813053765c4c9c3
zihang <tlswlgkd565@gmail.com>
2024-11-16 17:22:41 +0900

    feat : outbound에서 send 체크하는 기능 추가

--- src/main/java/LinkerBell/campus_market_spring/global/config/WebSocketConfig.java
+++ src/main/java/LinkerBell/campus_market_spring/global/config/WebSocketConfig.java
@@ -50,12 +50,30 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registration.interceptors(new ChannelInterceptor() {
             @Override
             public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
-                // Use SimpMessageHeaderAccessor to access STOMP headers
+                // Access headers using SimpMessageHeaderAccessor
                 SimpMessageHeaderAccessor accessor = SimpMessageHeaderAccessor.wrap(message);
+
+                // Log when a CONNECT_ACK is detected (if required)
                 if (accessor.getMessageType() == SimpMessageType.CONNECT_ACK) {
                     log.info("CONNECTED event detected. Session ID: {}", accessor.getSessionId());
+                }
+
+                // Log information for SEND type messages
+                if (accessor.getMessageType() == SimpMessageType.MESSAGE) {
+                    String destination = accessor.getDestination();
+                    String sessionId = accessor.getSessionId();
+                    Object payload = message.getPayload();
+
+                    // Attempt to get the user information (if authenticated)
+                    String username =
+                        accessor.getUser() != null ? accessor.getUser().getName() : "Unknown";
+
+                    log.info(
+                        "SEND event: User [{}], Session ID [{}], Destination [{}], Payload [{}]",
+                        username, sessionId, destination, payload);
                 }
             }
         });
     }
+
 }
