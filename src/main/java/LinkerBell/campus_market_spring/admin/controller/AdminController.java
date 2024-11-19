7cfeee81e5a58baf42437085baed11f703e9014c
kwon204 <dafeg3@naver.com>
2024-11-19 16:39:25 +0900

    test: 관리자 api 테스트 코드 추가
    관리자 api 테스트 코드 추가

--- src/main/java/LinkerBell/campus_market_spring/admin/controller/AdminController.java
+++ src/main/java/LinkerBell/campus_market_spring/admin/controller/AdminController.java
@@ -3,8 +3,11 @@ package LinkerBell.campus_market_spring.admin.controller;
 import LinkerBell.campus_market_spring.admin.dto.AdminLoginRequestDto;
 import LinkerBell.campus_market_spring.admin.service.AdminService;
 import LinkerBell.campus_market_spring.dto.AuthResponseDto;
+import LinkerBell.campus_market_spring.dto.AuthUserDto;
+import LinkerBell.campus_market_spring.global.auth.Login;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,10 @@ public class AdminController {
     public ResponseEntity<AuthResponseDto> adminLogin(@RequestBody AdminLoginRequestDto requestDto) {
         AuthResponseDto authResponseDto = adminService.adminLogin(requestDto.idToken());
         return ResponseEntity.ok(authResponseDto);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(@Login AuthUserDto user) {
+        return ResponseEntity.ok("Hello Admin! " + user.getLoginEmail());
     }
 }
