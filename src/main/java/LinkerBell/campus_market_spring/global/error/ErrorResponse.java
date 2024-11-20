ae61b557f7492961038f210198d43f435fcae041
kwon204 <dafeg3@naver.com>
2024-11-20 22:18:24 +0900

    fix: ErrorResponse 수정
    HttpStatus는  중복된 데이터로 삭제됨
    message와 code를 사용한 생성자 추가

--- src/main/java/LinkerBell/campus_market_spring/global/error/ErrorResponse.java
+++ src/main/java/LinkerBell/campus_market_spring/global/error/ErrorResponse.java
@@ -6,13 +6,21 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class ErrorResponse {
 
-    private final HttpStatus httpStatus;
     private final int code;
     private final String message;
 
     public ErrorResponse(ErrorCode errorCode) {
-        this.httpStatus = errorCode.getHttpStatus();
         this.code = errorCode.getCode();
         this.message = errorCode.getMessage();
+    }
+
+    public ErrorResponse(String message, int code) {
+        this.message = message;
+        this.code = code;
+    }
+
+    public ErrorResponse(String message) {
+        this.message = message;
+        this.code = 6666;
     }
 }
--- src/main/java/LinkerBell/campus_market_spring/global/error/GlobalExceptionHandler.java
+++ src/main/java/LinkerBell/campus_market_spring/global/error/GlobalExceptionHandler.java
@@ -19,9 +19,10 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleException(CustomException exception) {
-        ErrorResponse errorResponse = new ErrorResponse(exception.getErrorCode());
+        ErrorCode errorCode = exception.getErrorCode();
+        ErrorResponse errorResponse = new ErrorResponse(errorCode);
         log.info("Error : " + errorResponse.getMessage());
-        return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+        return new ResponseEntity<>(errorResponse, errorCode.getHttpStatus());
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
@@ -31,15 +32,15 @@ public class GlobalExceptionHandler {
         if (ex.getName().equals("minPrice") || ex.getName().equals("maxPrice")) {
             ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_PRICE);
             log.error(ErrorCode.INVALID_PRICE.getMessage());
-            return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+            return new ResponseEntity<>(errorResponse, ErrorCode.INVALID_PRICE.getHttpStatus());
         } else if (ex.getName().equals("category")) {
             ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_CATEGORY);
             log.error(ErrorCode.INVALID_CATEGORY.getMessage());
-            return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+            return new ResponseEntity<>(errorResponse, ErrorCode.INVALID_CATEGORY.getHttpStatus());
         } else if (ex.getName().equals("itemId")) {
             ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_ITEM_ID);
             log.error(ErrorCode.INVALID_ITEM_ID.getMessage());
-            return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+            return new ResponseEntity<>(errorResponse, ErrorCode.INVALID_ITEM_ID.getHttpStatus());
         }
 
         throw ex;
@@ -52,31 +53,31 @@ public class GlobalExceptionHandler {
         if (Objects.requireNonNull(ex.getFieldError()).getField().equals("title")) {
             ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_TITLE);
             log.error(ErrorCode.INVALID_TITLE.getMessage());
-            return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+            return new ResponseEntity<>(errorResponse, ErrorCode.INVALID_TITLE.getHttpStatus());
         } else if (Objects.requireNonNull(ex.getFieldError()).getField().equals("price")) {
             ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_PRICE);
             log.error(ErrorCode.INVALID_PRICE.getMessage());
-            return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+            return new ResponseEntity<>(errorResponse, ErrorCode.INVALID_PRICE.getHttpStatus());
         } else if (Objects.requireNonNull(ex.getFieldError()).getField().equals("description")) {
             ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_DESCRIPTION);
             log.error(ErrorCode.INVALID_DESCRIPTION.getMessage());
-            return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+            return new ResponseEntity<>(errorResponse, ErrorCode.INVALID_DESCRIPTION.getHttpStatus());
         } else if (Objects.requireNonNull(ex.getFieldError()).getField().equals("thumbnail")) {
             ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_THUMBNAIL);
             log.error(ErrorCode.INVALID_THUMBNAIL.getMessage());
-            return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+            return new ResponseEntity<>(errorResponse, ErrorCode.INVALID_THUMBNAIL.getHttpStatus());
         } else if (Objects.requireNonNull(ex.getFieldError()).getField().startsWith("images")) {
             ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_ITEM_PHOTOS);
             log.error(ErrorCode.INVALID_ITEM_PHOTOS.getMessage());
-            return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+            return new ResponseEntity<>(errorResponse, ErrorCode.INVALID_ITEM_PHOTOS.getHttpStatus());
         } else if (Objects.requireNonNull(ex.getFieldError()).getField().equals("itemStatus")) {
             ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_ITEM_STATUS);
             log.error(ErrorCode.INVALID_ITEM_STATUS.getMessage());
-            return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+            return new ResponseEntity<>(errorResponse, ErrorCode.INVALID_ITEM_STATUS.getHttpStatus());
         } else if (Objects.requireNonNull(ex.getFieldError()).getField().equals("buyerId")) {
             ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_ITEM_BUYER);
             log.error(ErrorCode.INVALID_ITEM_BUYER.getMessage());
-            return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+            return new ResponseEntity<>(errorResponse, ErrorCode.INVALID_ITEM_BUYER.getHttpStatus());
         }
 
         throw ex;
@@ -89,19 +90,19 @@ public class GlobalExceptionHandler {
                 .contains("LinkerBell.campus_market_spring.domain.Category")) {
                 ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_CATEGORY);
                 log.error(ErrorCode.INVALID_CATEGORY.getMessage());
-                return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+                return new ResponseEntity<>(errorResponse, ErrorCode.INVALID_CATEGORY.getHttpStatus());
             } else if (ex.getCause().getMessage()
                 .contains("LinkerBell.campus_market_spring.domain.ItemStatus")) {
                 ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_ITEM_STATUS);
                 log.error(ErrorCode.INVALID_ITEM_STATUS.getMessage());
-                return new ResponseEntity<>(errorResponse, errorResponse.getHttpStatus());
+                return new ResponseEntity<>(errorResponse, ErrorCode.INVALID_ITEM_STATUS.getHttpStatus());
             }
         }
         throw ex;
     }
 
     @ExceptionHandler(HandlerMethodValidationException.class)
-    public ResponseEntity handleHandlerMethodValidationException(
+    public ResponseEntity<ErrorResponse> handleHandlerMethodValidationException(
         HandlerMethodValidationException ex, HttpServletRequest request) {
 
         log.error("[{}], [{}], [{}], [{}]",
@@ -109,14 +110,14 @@ public class GlobalExceptionHandler {
             request.getRequestURI());
         if (ex.getMethod().getName().equals("getPresignedPutUrl")) {
             ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_FILE_NAME);
-            return ResponseEntity.status(errorResponse.getHttpStatus()).body(errorResponse);
+            return ResponseEntity.status(ErrorCode.INVALID_FILE_NAME.getHttpStatus()).body(errorResponse);
         }
 
-        return ResponseEntity.status(ex.getStatusCode()).body(ex.getBody());
+        return ResponseEntity.status(ex.getStatusCode()).body(new ErrorResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity handleMissingServletRequestParameterException(
+    public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(
         MissingServletRequestParameterException ex, HttpServletRequest request) {
 
         log.error("[{}], [{}], [{}], [{}]",
@@ -124,9 +125,9 @@ public class GlobalExceptionHandler {
             request.getRequestURI());
         if (ex.getMethodParameter().getMethod().getName().equals("getPresignedPutUrl")) {
             ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INVALID_FILE_NAME);
-            return ResponseEntity.status(errorResponse.getHttpStatus()).body(errorResponse);
+            return ResponseEntity.status(ErrorCode.INVALID_FILE_NAME.getHttpStatus()).body(errorResponse);
         }
 
-        return ResponseEntity.status(ex.getStatusCode()).body(ex.getBody());
+        return ResponseEntity.status(ex.getStatusCode()).body(new ErrorResponse(ex.getMessage()));
     }
 }
