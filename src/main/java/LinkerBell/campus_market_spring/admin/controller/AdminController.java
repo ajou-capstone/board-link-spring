18d15ee85eca253efb9fb7b2c7494fe70b285635
kwon204 <dafeg3@naver.com>
2024-11-22 16:44:57 +0900

    feat: 관리자용 상품 신고 목록 보기 기능 v1

--- src/main/java/LinkerBell/campus_market_spring/admin/controller/AdminController.java
+++ src/main/java/LinkerBell/campus_market_spring/admin/controller/AdminController.java
@@ -1,16 +1,22 @@
 package LinkerBell.campus_market_spring.admin.controller;
 
 import LinkerBell.campus_market_spring.admin.dto.AdminLoginRequestDto;
+import LinkerBell.campus_market_spring.admin.dto.ItemReportSearchResponseDto;
 import LinkerBell.campus_market_spring.admin.service.AdminService;
 import LinkerBell.campus_market_spring.dto.AuthResponseDto;
 import LinkerBell.campus_market_spring.dto.AuthUserDto;
+import LinkerBell.campus_market_spring.dto.SliceResponse;
 import LinkerBell.campus_market_spring.global.auth.Login;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,5 +35,13 @@ public class AdminController {
     @GetMapping("/test")
     public ResponseEntity<String> test(@Login AuthUserDto user) {
         return ResponseEntity.ok("Hello Admin! " + user.getLoginEmail());
+    }
+
+    @GetMapping("/items/report")
+    public ResponseEntity< SliceResponse<ItemReportSearchResponseDto>> getItemReports(
+        @PageableDefault(page = 0, size = 0, sort = "createDate",
+        direction = Direction.DESC)Pageable pageable) {
+        SliceResponse<ItemReportSearchResponseDto> response = adminService.getItemReports(pageable);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/LinkerBell/campus_market_spring/admin/service/AdminService.java
+++ src/main/java/LinkerBell/campus_market_spring/admin/service/AdminService.java
@@ -1,15 +1,22 @@
 package LinkerBell.campus_market_spring.admin.service;
 
+import LinkerBell.campus_market_spring.admin.dto.ItemReportSearchResponseDto;
+import LinkerBell.campus_market_spring.domain.ItemReport;
 import LinkerBell.campus_market_spring.domain.Role;
 import LinkerBell.campus_market_spring.domain.User;
 import LinkerBell.campus_market_spring.dto.AuthResponseDto;
+import LinkerBell.campus_market_spring.dto.SliceResponse;
 import LinkerBell.campus_market_spring.global.error.ErrorCode;
 import LinkerBell.campus_market_spring.global.error.exception.CustomException;
 import LinkerBell.campus_market_spring.global.jwt.JwtUtils;
+import LinkerBell.campus_market_spring.repository.ItemReportRepository;
 import LinkerBell.campus_market_spring.repository.UserRepository;
 import LinkerBell.campus_market_spring.service.GoogleAuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,6 +27,7 @@ public class AdminService {
     private final GoogleAuthService googleAuthService;
     private final UserRepository userRepository;
     private final JwtUtils jwtUtils;
+    private final ItemReportRepository itemReportRepository;
 
     public AuthResponseDto adminLogin(String idToken) {
         String email = googleAuthService.getEmailWithVerifyIdToken(idToken);
@@ -40,5 +48,11 @@ public class AdminService {
         return AuthResponseDto.builder()
             .accessToken(accessToken)
             .refreshToken(refreshToken).build();
+    }
+
+    public SliceResponse<ItemReportSearchResponseDto> getItemReports(Pageable pageable) {
+        Slice<ItemReport> itemReports = itemReportRepository.findItemReports(pageable);
+
+        return new SliceResponse<>(itemReports.map(ItemReportSearchResponseDto::new));
     }
 }
