673e7365f216450b36547ff44809fa4307eb9ed4
kwon204 <dafeg3@naver.com>
2024-11-22 14:15:10 +0900

    fix: QaRequestDto validation 추가

--- src/main/java/LinkerBell/campus_market_spring/dto/QaRequestDto.java
+++ src/main/java/LinkerBell/campus_market_spring/dto/QaRequestDto.java
@@ -1,9 +1,13 @@
 package LinkerBell.campus_market_spring.dto;
 
 import LinkerBell.campus_market_spring.domain.QaCategory;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record QaRequestDto(String title,
-                           QaCategory category,
-                           String description) {
+public record QaRequestDto(
+    @NotBlank @Size(min = 2, max = 100, message = "문의 제목은 2자 이상 100자 이하입니다.") String title,
+    @NotNull QaCategory category,
+    @NotBlank @Size(min = 2, max = 1000, message = "문의 내용은 2자 이상 1000자 이하입니다.") String description) {
 
 }
