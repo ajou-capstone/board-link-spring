b86a2c55cfe84bf706b47cb4bbbb3cfdebc2aa16
zihang <tlswlgkd565@gmail.com>
2024-11-23 20:01:09 +0900

    fix : 리뷰 목록을 가져오는 기능 추가

--- src/main/java/LinkerBell/campus_market_spring/controller/ReviewController.java
+++ src/main/java/LinkerBell/campus_market_spring/controller/ReviewController.java
@@ -2,11 +2,17 @@ package LinkerBell.campus_market_spring.controller;
 
 import LinkerBell.campus_market_spring.dto.AuthUserDto;
 import LinkerBell.campus_market_spring.dto.ReviewRequestDto;
+import LinkerBell.campus_market_spring.dto.ReviewResponseDto;
+import LinkerBell.campus_market_spring.dto.SliceResponse;
 import LinkerBell.campus_market_spring.global.auth.Login;
 import LinkerBell.campus_market_spring.service.ReviewService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +33,17 @@ public class ReviewController {
         reviewService.postReview(authUserDto.getUserId(), userId, reviewRequestDto);
 
         return ResponseEntity.noContent().build();
+    }
+
+    //리뷰 가져오기
+    @GetMapping("/api/v1/users/{userId}/reviews")
+    public ResponseEntity<SliceResponse<ReviewResponseDto>> getReviews(
+        @Login AuthUserDto authUserDto,
+        @PageableDefault(page = 0, size = 10, sort = "createdDate", direction = Sort.Direction.DESC) Pageable pageable) {
+        SliceResponse<ReviewResponseDto> reviewResponse = reviewService.getReviews(
+            authUserDto.getUserId(), pageable);
+
+        return ResponseEntity.ok(reviewResponse);
     }
 
 }
