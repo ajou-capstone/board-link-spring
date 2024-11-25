f1944159cad3e4f178e9d2122fe2cce0b6114b6f
zihang <tlswlgkd565@gmail.com>
2024-11-25 20:43:24 +0900

    fix : createdDate가 같을 시 itemId기준으로 정렬하게 변경

--- src/main/java/LinkerBell/campus_market_spring/controller/DealHistoryController.java
+++ src/main/java/LinkerBell/campus_market_spring/controller/DealHistoryController.java
@@ -10,7 +10,10 @@ import LinkerBell.campus_market_spring.service.DealHistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
+import org.springframework.data.web.SortDefault.SortDefaults;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +30,9 @@ public class DealHistoryController {
     public ResponseEntity<SliceResponse<DealHistoryResponseDto>> getDealHistory(
         @Login AuthUserDto authUserDto, @PathVariable("userId") Long userId,
         @RequestParam String type,
-        @PageableDefault(page = 0, size = 10, sort = "createdDate", direction = Sort.Direction.DESC) Pageable pageable) {
+        @PageableDefault(page = 0, size = 10, sort = "createdDate", direction = Sort.Direction.DESC) @SortDefaults({
+            @SortDefault(sort = "createdDate", direction = Direction.DESC),
+            @SortDefault(sort = "itemId", direction = Direction.DESC)}) Pageable pageable) {
         SliceResponse<DealHistoryResponseDto> dealHistoryResponse;
 
         if (type.equals("all")) {
