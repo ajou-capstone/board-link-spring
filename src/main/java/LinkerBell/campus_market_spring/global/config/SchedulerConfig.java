877fb64f79ec1e3209cd17a48ad4e5b78ce78f0b
kwon204 <dafeg3@naver.com>
2024-11-25 20:20:25 +0900

    feat: 스케쥴러 추가
    매일 자정에 정지 기간이 끝난 사용자를 지우는 스케줄러 추가

---
+++ src/main/java/LinkerBell/campus_market_spring/global/config/SchedulerConfig.java
@@ -0,0 +1,28 @@
+package LinkerBell.campus_market_spring.global.config;
+
+import LinkerBell.campus_market_spring.domain.Blacklist;
+import LinkerBell.campus_market_spring.repository.BlacklistRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+@Transactional
+public class SchedulerConfig {
+
+    private final BlacklistRepository blacklistRepository;
+
+    @Scheduled(cron = "${cron.recovery}")
+    public void recoveryUser() {
+        LocalDateTime now = LocalDateTime.now();
+        List<Blacklist> blacklists = blacklistRepository.findAll();
+        List<Blacklist> userList = blacklists.stream().filter(blacklist -> {
+            return blacklist.getEndDate().isBefore(now);
+        }).toList();
+        blacklistRepository.deleteAll(userList);
+    }
+}
