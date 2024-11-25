ad3b597dc95b16d5b3fc3e1a46f04770ed59de4e
kwon204 <32539398+kwon204@users.noreply.github.com>
2024-11-25 21:17:34 +0900

    feat: 스케쥴러 추가 (#136)

---
+++ src/main/java/LinkerBell/campus_market_spring/global/config/SchedulerConfig.java
@@ -0,0 +1,32 @@
+package LinkerBell.campus_market_spring.global.config;
+
+import LinkerBell.campus_market_spring.domain.Blacklist;
+import LinkerBell.campus_market_spring.repository.BlacklistRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class SchedulerConfig {
+
+    private final BlacklistRepository blacklistRepository;
+
+    @Scheduled(cron = "${cron.recovery}")
+    public void recoveryUser() {
+        log.debug("recovery scheduled start...");
+        LocalDateTime now = LocalDateTime.now();
+        List<Blacklist> blacklists = blacklistRepository.findAll();
+        List<Blacklist> userList = blacklists.stream().filter(blacklist -> {
+            return blacklist.getEndDate().isBefore(now);
+        }).toList();
+        blacklistRepository.deleteAll(userList);
+        log.debug("recovery scheduled finish...");
+    }
+}
