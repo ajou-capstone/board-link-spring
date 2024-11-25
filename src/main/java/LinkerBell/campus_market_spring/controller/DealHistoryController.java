e5a2334e4f5dd4d75edef86cabe69c077019b957
zihang <tlswlgkd565@gmail.com>
2024-11-25 20:28:57 +0900

    feat : 거래 내역을 가져오는 기능 추가

---
+++ src/main/java/LinkerBell/campus_market_spring/controller/DealHistoryController.java
@@ -0,0 +1,47 @@
+package LinkerBell.campus_market_spring.controller;
+
+import LinkerBell.campus_market_spring.dto.AuthUserDto;
+import LinkerBell.campus_market_spring.dto.DealHistoryResponseDto;
+import LinkerBell.campus_market_spring.dto.SliceResponse;
+import LinkerBell.campus_market_spring.global.auth.Login;
+import LinkerBell.campus_market_spring.global.error.ErrorCode;
+import LinkerBell.campus_market_spring.global.error.exception.CustomException;
+import LinkerBell.campus_market_spring.service.DealHistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DealHistoryController {
+
+    private final DealHistoryService dealHistoryService;
+
+    @GetMapping("/api/v1/items/{userId}/history")
+    public ResponseEntity<SliceResponse<DealHistoryResponseDto>> getDealHistory(
+        @Login AuthUserDto authUserDto, @PathVariable("userId") Long userId,
+        @RequestParam String type,
+        @PageableDefault(page = 0, size = 10, sort = "createdDate", direction = Sort.Direction.DESC) Pageable pageable) {
+        SliceResponse<DealHistoryResponseDto> dealHistoryResponse;
+
+        if (type.equals("all")) {
+            dealHistoryResponse = dealHistoryService.getAllDealHistory(authUserDto.getUserId(),
+                userId, pageable);
+        } else if (type.equals("purchase")) {
+            dealHistoryResponse = dealHistoryService.getPurchaseDealHistory(authUserDto.getUserId(),
+                userId, pageable);
+        } else if (type.equals("sales")) {
+            dealHistoryResponse = dealHistoryService.getSalesDealHistory(userId, pageable);
+        } else {
+            throw new CustomException(ErrorCode.INVALID_DEAL_TYPE);
+        }
+
+        return ResponseEntity.ok(dealHistoryResponse);
+    }
+}
--- src/main/java/LinkerBell/campus_market_spring/repository/ItemRepository.java
+++ src/main/java/LinkerBell/campus_market_spring/repository/ItemRepository.java
@@ -1,7 +1,10 @@
 package LinkerBell.campus_market_spring.repository;
 
 import LinkerBell.campus_market_spring.domain.Item;
+import LinkerBell.campus_market_spring.domain.User;
 import java.time.LocalDateTime;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +18,15 @@ public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositor
     @Query("update Item i set i.createdDate = :createdDate where i.itemId = :itemId")
     void updateCreatedDate(@Param("itemId") Long itemId,
         @Param("createdDate") LocalDateTime createdDate);
+
+    @Query("SELECT i FROM Item i WHERE i.user = :requestedUser OR i.userBuyer = :requestedUser")
+    Slice<Item> findAllHistoryByUser(@Param("requestedUser") User requestedUser, Pageable pageable);
+
+    @Query("SELECT i FROM Item i WHERE i.userBuyer = :requestedUser")
+    Slice<Item> findPurchaseHistoryByUser(@Param("requestedUser") User requestedUser,
+        Pageable pageable);
+
+    @Query("SELECT i FROM Item i WHERE i.user = :requestedUser")
+    Slice<Item> findSalesHistoryByUser(@Param("requestedUser") User requestedUser,
+        Pageable pageable);
 }
