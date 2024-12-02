d0e60fd4077894e25fb814872f0c05625714d1b6
sms3025 <101379994+sms3025@users.noreply.github.com>
2024-12-02 21:19:35 +0900

    fix: fcm 비동기 알림 에러 해결 (#152)

--- src/main/java/LinkerBell/campus_market_spring/CampusMarketSpringApplication.java
+++ src/main/java/LinkerBell/campus_market_spring/CampusMarketSpringApplication.java
@@ -4,18 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
-@EnableAsync
 @EnableScheduling
 @SpringBootApplication
 public class CampusMarketSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CampusMarketSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CampusMarketSpringApplication.class, args);
+    }
 
 }
